d858eded5f3e1f4cf3171ae3a5736abfd663f2bb
Mark Friedenbach <mark@friedenbach.org>
2023-05-09 11:14:48 -0700

    Move menubar code from main.rs into the proper platform-dependent and independent modules.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
+pub mod menubar;
 pub mod platform;
 pub mod platform_impl;
+
+pub const APP_NAME: &str = "atomCAD";
 
 // End of File
